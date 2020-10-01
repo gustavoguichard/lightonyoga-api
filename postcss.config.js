e58515c70e4b1d544039cb3e124ad509e27167ba
Gustavo Guichard <gustavoguichard@gmail.com>
2020-09-30 23:00:10 -0300

    Add purgecss to production

--- postcss.config.js
+++ postcss.config.js
@@ -12,4 +12,17 @@ let environment = {
   ],
 }
 
+if (process.env.RAILS_ENV === 'production') {
+  environment.plugins.push(
+    require('@fullhuman/postcss-purgecss')({
+      content: [
+        './app/**/.html.erb',
+        './app/helpers/**/*.rb',
+        './app/javascript/**/*.js',
+      ],
+      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+    }),
+  )
+}
+
 module.exports = environment
